6c7e6c6b065385fb7654cbcac8444a52c36957dc
mohamedAlsayed <m.alsayed.salah@gmail.com>
2015-06-22 17:46:27 +0200

    mainly statisify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
-    <title>title</title>
-</head>
-<body>
-<div class="container">
+@extends('layout.master')
+@section('title')
+    main title
+@stop
+@section('content')
     content
-</div>
-footer
-</body>
-</html>
+    @stop
+@section('footer')
+    copyright 2015
+    @stop
